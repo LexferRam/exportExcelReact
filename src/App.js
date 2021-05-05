5c1f119bc3a023b2b09925057f131cf7d1b79861
Lexfer Ramirez <lexferramirez@gmail.com>
2021-05-04 21:07:05 -0400

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExportExcel from "react-export-excel";
+
+const ExcelFile = ExportExcel.ExcelFile;
+const ExcelSheet = ExportExcel.ExcelSheet;
+const ExcelColumn = ExportExcel.ExcelColumn;
+
+const json = [
+  { nombre: "Lexfer", apellido: "Ramirez" },
+  { nombre: "Lexfer1", apellido: "Ramirez1" },
+  { nombre: "Lexfer2", apellido: "Ramirez2" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExcelFile
+        element={<button>Exportar Excel</button>}
+        filename="Prueba Lexfer"
+      >
+        <ExcelSheet data={json} name="Json de lexfer">
+          <ExcelColumn label="nombre" value="nombre" />
+          <ExcelColumn label="apellido" value="apellido" />
+        </ExcelSheet>
+      </ExcelFile>
     </div>
   );
 }
